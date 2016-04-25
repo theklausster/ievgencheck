b9e1bb06f5e1e3031970c7ca694b44b9325e8457
jk_km@ukr.net <jk_km@ukr.net>
2016-04-25 10:43:42 +0200

    Jpanel added

---
+++ PC/src/MainPanel.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.geom.Ellipse2D;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Stepanenko
+ */
+public class MainPanel extends JPanel{
+    private final int ELLIPSE_RADIUS = 3;
+    List pointList;
+    List obstaclesList;
+    Color pointColor;
+    Color obstaclesColor;
+    int width;
+    int height;
+    double scale;
+
+    public MainPanel() {
+        pointList = new ArrayList();
+        obstaclesList = new ArrayList();
+        pointColor = Color.BLUE;
+        obstaclesColor = Color.RED;
+        setBackground(Color.white);
+        width = getWidth();
+        height = getHeight();
+        scale = 1.0;
+    }
+
+   
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g); //To change body of generated methods, choose Tools | Templates.
+        Graphics2D g2 = (Graphics2D)g;
+        Point point;
+        Point nextPoint;
+        Ellipse2D ellipse;
+        Ellipse2D nextEllipse;
+        g2.setPaint(Color.black);
+        //coordinate
+        g2.drawLine(0, height/2, width, height/2);
+        g2.drawLine(width/2, 0, width/2, height);
+        
+        for(int j = 0; j < pointList.size(); j++)
+        {
+            point = (Point) pointList.get(j);
+            ellipse = new Ellipse2D.Double(point.x - ELLIPSE_RADIUS, point.y - ELLIPSE_RADIUS, 2*ELLIPSE_RADIUS, 2*ELLIPSE_RADIUS);
+            g2.setPaint(pointColor);
+            g2.fill(ellipse);
+            
+            if ( j != pointList.size() - 2 ){
+                nextPoint = (Point) pointList.get(j+1);
+                nextEllipse = new Ellipse2D.Double(nextPoint.x - ELLIPSE_RADIUS, nextPoint.y - ELLIPSE_RADIUS, 2*ELLIPSE_RADIUS, 2*ELLIPSE_RADIUS);
+                g2.drawLine(point.x,point.y,nextPoint.x,nextPoint.y);
+            }
+        }
+        
+        for(int j = 0; j < obstaclesList.size(); j++)
+        {
+            point = (Point) obstaclesList.get(j);
+            ellipse = new Ellipse2D.Double(point.x - ELLIPSE_RADIUS, point.y - ELLIPSE_RADIUS, 2*ELLIPSE_RADIUS, 2*ELLIPSE_RADIUS);
+            g2.setPaint(obstaclesColor);
+            g2.fill(ellipse);
+         
+        }
+    }
+    
+    public void addPoint(double x, double y)
+    {
+        Point p = convertToPoint(x,y);
+        pointList.add(p);
+        repaint();
+    }    
+    
+    public void addObstacles(double x, double y){
+        Point p = convertToPoint(x,y);
+        obstaclesList.add(p);
+        repaint();
+    }
+
+    private Point convertToPoint(double x, double y) {
+        int roundedX = (int) Math.round(x);
+        int roundedY = (int) Math.round(y);
+        //turn coordinate 90 degrees
+        int normalX = -roundedY;
+        int normalY = roundedX;
+        //convert x,y to JPanel coordinate
+        Point p = new Point(normalX + width/2, height/2 - normalY);
+        return p;
+    }
+}
